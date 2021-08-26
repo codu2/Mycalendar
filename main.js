f038a3f89d2758c70d26d8eb086244e5cf54cc7b
codu2 <82283517+codu2@users.noreply.github.com>
2021-08-26 20:41:28 +0900

    Update Javascript
    show border-bottom when selecting a date, change JavaScript to work properly, show days and dates for a week, including today in the todo list, add todo checkboxes, add todo modifications entered and features that can be deleted, and add features that can delete all todo

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-document.querySelector('.dark-mode-switch').onclick = () => {
-    document.querySelector('body').classList.toggle('dark');
-    document.querySelector('body').classList.toggle('light');
-    if (document.querySelector('body').classList.contains('dark')) {
-        document.querySelector('.toggle span').textContent = 'Light Mode';
-    } else if (document.querySelector('body').classList.contains('light')) {
-        document.querySelector('.toggle span').textContent = 'Dark Mode';
-    }
-}
-
 let calendar = document.querySelector('.calendar');
 
 const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -98,3 +88,140 @@ let curr_year = { value: currDate.getFullYear() };
 
 generateCalendar(curr_month.value, curr_year.value);
 
+let todo_today = document.querySelector('.todo-today');
+let todo_days = document.querySelector('.todo-days');
+let todo_week_day = document.querySelector('.todo-week-day');
+let first_day = new Date(currDate.getFullYear(), currDate.getMonth(), 1);
+let last_day = new Date(currDate.getFullYear(), currDate.getMonth() + 1, 0);
+const month_today = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let days_of_month = [31, getFebDay(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+todo_today.innerHTML = `${month_names[currDate.getMonth()]} ${currDate.getFullYear()}`;
+
+for (let i = 0; i < 7; i++) {
+    let todo_day = document.createElement('div');
+    if (i + currDate.getDate() <= last_day.getDate()) {
+    todo_day.innerHTML = i + currDate.getDate();
+    } else if (i + currDate.getDate() > last_day.getDate()) {
+    todo_day.innerHTML = i + currDate.getDate() - last_day.getDate();
+    }
+    todo_days.appendChild(todo_day);
+
+    let todo_week = document.createElement('div');
+    if (currDate.getDay() + i > 6) {
+    todo_week.innerHTML = `${month_today[i - 3]}`;
+    } else {
+    todo_week.innerHTML = `${month_today[currDate.getDay() + i]}`;
+    }
+    todo_week_day.appendChild(todo_week);
+
+    todo_day.onclick = () => todo_day.classList.toggle('todo-day-click');
+}
+
+const inputBox = document.querySelector('.todo-input input');
+const addBtn = document.querySelector('.todo-input button');
+const todoList = document.querySelector('.todo-list');
+const deleteAllBtn = document.querySelector('.todo-footer button');
+
+inputBox.onkeyup = () => {
+    let userData = inputBox.value;
+    if (userData.trim() != 0) {
+        addBtn.classList.add('active');
+    } else {
+        addBtn.classList.remove('active');
+    }
+}
+
+showTasks();
+
+addBtn.onclick = () => {
+    let getLocalStorage = localStorage.getItem("New Todo");
+    if (getLocalStorage == null) {
+        listArr = [];
+    } else {
+        listArr = JSON.parse(getLocalStorage);
+    }
+
+    let userData = inputBox.value;
+    listArr.push(userData);
+    localStorage.setItem("New Todo", JSON.stringify(listArr));
+
+    showTasks();
+}
+
+function showTasks() {
+    let getLocalStorage = localStorage.getItem("New Todo");
+    if (getLocalStorage == null) {
+        listArr = [];
+    } else {
+        listArr = JSON.parse(getLocalStorage);
+    }
+
+    const pendingNumb = document.querySelector('.pendingNumb');
+    pendingNumb.textContent = listArr.length;
+    /*innerText는 불필요한 공백을 제거하고 텍스트로 반환하지만
+    textContent는 모든 텍스트를 그대로 가져옴*/
+
+    if (listArr.length > 0) {
+        deleteAllBtn.classList.add('active');
+    } else {
+        deleteAllBtn.classList.remove('active');
+    }
+
+    let newLiTag = '';
+    listArr.forEach((element, index) => {
+        newLiTag += `<li>
+                        <input type="checkbox"><label class="label">${element}</label>
+                        <input type="text" value="${element}">
+                        <div class="todo-list-icon">
+                            <span class="edit" onclick="editTask(${index})"><i class="ri-edit-2-line"></i></span>
+                            <span onclick="deleteTask(${index})"><i class="ri-delete-bin-line"></i></span>
+                        </div>
+                    </li>`;
+    });
+
+    todoList.innerHTML = newLiTag;
+    inputBox.value = '';
+}
+
+function deleteTask(index) {
+    let getLocalStorage = localStorage.getItem("New Todo");
+    listArr = JSON.parse(getLocalStorage);
+    listArr.splice(index, 1);
+
+    localStorage.setItem("New Todo", JSON.stringify(listArr));
+    showTasks();
+}
+
+deleteAllBtn.onclick = () => {
+    listArr = [];
+    localStorage.setItem("New Todo", JSON.stringify(listArr));
+    showTasks();
+}
+
+function editTask(index) {
+    let edit = document.querySelectorAll('.edit')[index];
+    let listItem = edit.parentNode.parentNode; //li
+    
+    let editInput = listItem.querySelector('input[type=text]');
+    let label = listItem.querySelector('label');
+    let containsClass = listItem.classList.contains('editMode');
+
+    if(containsClass) {
+        label.innerText = editInput.value;
+    } else {
+        editInput.value = label.innerText;
+    }
+    
+    listItem.classList.toggle('editMode');
+}
+
+document.querySelector('.dark-mode-switch').onclick = () => {
+    document.querySelector('body').classList.toggle('dark');
+    document.querySelector('body').classList.toggle('light');
+    if (document.querySelector('body').classList.contains('dark')) {
+        document.querySelector('.toggle span').textContent = 'Light Mode';
+    } else if (document.querySelector('body').classList.contains('light')) {
+        document.querySelector('.toggle span').textContent = 'Dark Mode';
+    }
+}
